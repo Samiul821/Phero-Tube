f8d66e91bebf2e9dcaa944c3c87726a98609d5d9
Samiul821 <smsamiul821@gmail.com>
2025-03-11 22:54:14 +0600

    dynamic btn create

--- Js/index.js
+++ Js/index.js
@@ -2,11 +2,33 @@ function loadCategories() {
   // fetch the data
 
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    //  2 - convart promise to json
     .then((res) => res.json())
     .then((data) => displayCategories(data.categories));
 }
+
+// {
+//     "category_id": "1001",
+//     "category": "Music"
+//     }
+
 function displayCategories(categories) {
-  console.log(categories);
+  //   get the container
+  const categoriyContainer = document.getElementById("categoriy-container");
+
+  //   Loop operation on Array of object
+  for (let cat of categories) {
+    console.log(cat);
+
+    // create Element
+    const categoriyDiv = document.createElement("div");
+    categoriyDiv.innerHTML = `
+    <button class="btn bg-[#25252515] hover:bg-[#FF1F3D] text-lg hover:text-white text-[#252525b2]">${cat.category}</button>
+    `;
+
+    // Append the Element
+    categoriyContainer.appendChild(categoriyDiv);
+  }
 }
 
 loadCategories();
